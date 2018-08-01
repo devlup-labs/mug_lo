032caf648a2a70f2838242a698f374725d3f0261
ajatprabha <ajat.prabha.leo@gmail.com>
2018-08-02 02:12:14 +0530

    update course models

--- src/courses/admin.py
+++ src/courses/admin.py
@@ -1,9 +1,26 @@
 from django.contrib import admin
-from .models import Course
+from .models import Course, MarksDistribution, CreditDistribution
+from courses.forms import MarksDistributionForm
 
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
     class Meta:
         model = Course
+        fields = '__all__'
+
+
+@admin.register(MarksDistribution)
+class MarksDistributionAdmin(admin.ModelAdmin):
+    form = MarksDistributionForm
+
+    class Meta:
+        model = MarksDistribution
+        fields = '__all__'
+
+
+@admin.register(CreditDistribution)
+class CreditDistributionAdmin(admin.ModelAdmin):
+    class Meta:
+        model = CreditDistribution
         fields = '__all__'
